2aa732b2e05894d49a57281c6d7c07be1974382a
jromero <ing.juliocesar26@outlook.com>
2024-08-05 21:12:35 -0600

    Se agregó la impresion de los numeros del 0 al 99 como se pidió en el feature .......

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,9 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        for(int i=0; i<100; i++){
+            System.out.println(i);
+        }
     }
 }
